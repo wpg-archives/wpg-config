6a5f046531c9d9523f0d6fee3b725ac4bebc8537
David DAUGREILH <david.daugreilh@globalis-ms.com>
2016-09-16 11:13:57 +0200

    Fix wp-config.php, add gitignore file, rename wp-config and wp-config-database

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -3,11 +3,11 @@
 define( 'WP_ENV',   'development' );
 define( 'WP_STAGE', 'development-<USERNAME>' );
 
+if ( ! defined( 'ABSPATH' ) )
+	define( 'ABSPATH', dirname(__FILE__) . '/' );
+
 require_once( ABSPATH . '/wp-config/wp-config-database.php' );
 require_once( ABSPATH . '/wp-config/wp-config-common.php' );
 require_once( ABSPATH . '/wp-config/wp-config-' . WP_ENV . '.php' );
-
-if ( ! defined( 'ABSPATH' ) )
-	define( 'ABSPATH', dirname(__FILE__) . '/' );
 
 require_once( ABSPATH . 'wp-settings.php' );
